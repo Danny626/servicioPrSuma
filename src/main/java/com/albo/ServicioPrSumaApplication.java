068240cd4a12ed87f62edfd195de3252e52947d7
Danny <live626@gmail.com>
2022-01-27 14:32:03 -0400

    inicio proyecto

---
+++ src/main/java/com/albo/ServicioPrSumaApplication.java
@@ -0,0 +1,13 @@
+package com.albo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServicioPrSumaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServicioPrSumaApplication.class, args);
+	}
+
+}
